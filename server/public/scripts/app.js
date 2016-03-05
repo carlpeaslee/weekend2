54ddb19e7c8bdad9e944554338f8e3937d14fbfc
Carl Peaslee <cpeaslee@gmail.com>
2016-03-04 23:29:07 -0600

    timer working

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
+
+  setInterval(incrementTimer,1000);
+
+  setInterval(checkTimer, 1000);
+
+  function checkTimer() {
+    if (timer >= 10) {
+      currentSlide++;
+      timer = 0;
+      if (currentSlide >= rawData.kappa.length) {
+        currentSlide = rawData.kappa.length;
+        $('.stage').html('<h1>Kappa Karousel</h1>');
+      } else {
+        $('.stage').empty();
+        createSlide(currentSlide);
+      };
+    }
+  }
+
+
   $('.next').on('click', function() {
     currentSlide++;
+    timer = 0;
     if (currentSlide >= rawData.kappa.length) {
       currentSlide = rawData.kappa.length;
       $('.stage').html('<h1>Kappa Karousel</h1>');
@@ -12,12 +33,14 @@ $(document).ready(function(){
 
   $('.previous').on('click', function() {
     currentSlide--;
+    timer = 0;
     if (currentSlide < 0) {
       currentSlide = -1;
-      $('.stage').html('<h1>Welcome to the Kappa Karousel</h1>');
+      $('.stage').html('<h1>Thats all of Kappa!</h1>');
     } else {
       $('.stage').empty();
       createSlide(currentSlide);
+      $('.progress-bar').attr('aria-valuenow', currentSlide + 1);
     };
   });
 
@@ -35,7 +58,7 @@ $.ajax({
 //variables
 var currentSlide = -1;
 var rawData;
-
+var timer = 0;
 
 //functions
 
@@ -47,4 +70,10 @@ function createSlide(i) {
   $('.stage').append('<h3>Shoutout: ' + rawData.kappa[i].shoutout + '</h3>').fadeIn();
   $('.progress-bar').attr('aria-valuenow', currentSlide + 1);
   $('.progress-bar').attr('style', "width: " + Math.floor(((currentSlide + 1) / rawData.kappa.length) * 100) + "%");
+}
+
+
+
+function incrementTimer() {
+  timer ++;
 }
